03a20dd3df52caf0e510ffbb59f53584a803019b
OPM-ver0 <157506530+OPM-ver0@users.noreply.github.com>
2025-10-11 23:23:57 +0600

    Create Que_02.dart

---
+++ Assignment 1/Que_02.dart
@@ -0,0 +1,9 @@
+// Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
+void main() {
+  //single quotes
+  print('Hello I am "John Doe"');
+  print( 'Hello I\'am "John Doe"');
+  //Double qutes
+  print("Hello I am \"John Doe\"");
+  print( "Hello I'am \"John Doe\"");
+}
