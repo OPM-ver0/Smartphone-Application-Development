eb491b249ea029ff579d9bcaa427a1ba6ae468b3
OPM-ver0 <157506530+OPM-ver0@users.noreply.github.com>
2025-10-23 23:43:16 +0600

    Create Que_01.dart

---
+++ Assignment 3/Practice question 4/Que_01.dart
@@ -0,0 +1,7 @@
+// Create a list of names and print all names using list.
+void main() {
+    List<String>s =['aniruddha','messi','msi abrar','nafi_king','akash_king'];
+    for(var i in s) {
+        print(i);
+    }
+}
